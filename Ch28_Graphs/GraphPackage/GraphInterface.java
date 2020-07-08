801bc6308db17e93b27576dd64292f0f82bf4a42
hryungk <38110685+hryungk@users.noreply.github.com>
2020-07-08 13:20:27 -0700

    Add files via upload

---
+++ Ch28_Graphs/GraphPackage/GraphInterface.java
@@ -0,0 +1,12 @@
+package GraphPackage;
+/**
+   An interface of methods that create, manipulate, and process a graph.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 5.0
+*/
+public interface GraphInterface<T> extends BasicGraphInterface<T>, 
+                                           GraphAlgorithmsInterface<T>
+{
+} // end GraphInterface
